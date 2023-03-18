6d4528063af2818a15c5d7ddbd1239c7cbb6e5cc
Marcus Blättermann <marcus@essenmitsosse.de>
2023-03-18 21:15:33 +0100

    Delete unused content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
-import Image from 'next/image'
 import './globals.css'
-import styles from './page.module.css'
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,34 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <main className={styles.main}>
-          <div className={styles.description}>
-            <p>
-              Get started by editing&nbsp;
-              <code className={styles.code}>app/page.tsx</code>
-            </p>
-            <div>
-              <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                By{' '}
-                <Image
-                  src="/vercel.svg"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
-                  width={100}
-                  height={24}
-                  priority
-                />
-              </a>
-            </div>
-          </div>
-          {children}
-        </main>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
